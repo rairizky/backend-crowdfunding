b1def598fde74e2358e94a825484071a81fa1dc8
Rafli Rai Rizky <rairizky37@gmail.com>
2021-01-23 18:26:45 +0700

    [test] fetch & return json

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
-import(
-	"fmt"
+import (
+	"bwastartup/user"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:root@tcp(127.0.0.1:8889)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connection to database is good")
+	var users []user.User
+	db.Find(&users)
+
+	// pointer users to json
+	c.JSON(http.StatusOK, users)
 }

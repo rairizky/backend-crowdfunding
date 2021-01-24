59ba8079045d94cd3897aa6a9d97b1b08ea3eecd
Rafli Rai Rizky <rairizky37@gmail.com>
2021-01-24 12:29:35 +0700

    [add] service input register

--- main.go
+++ main.go
@@ -3,21 +3,11 @@ package main
 import (
 	"bwastartup/user"
 	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:root@tcp(127.0.0.1:8889)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -25,9 +15,16 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	
+	userInput := user.RegisterUserInput{}
+	userInput.Name = "Test Simpan dari service"
+	userInput.Email = "contoh@gmail.com"
+	userInput.Occupation = "anak band"
+	userInput.Password = "password"
 
-	// pointer users to json
-	c.JSON(http.StatusOK, users)
+	userService.RegisterUser(userInput)
 }
+
+
---
+++ user/input.go
@@ -0,0 +1,9 @@
+package user
+
+type RegisterUserInput struct {
+	Name string
+	Occupation string
+	Email string
+	Password string
+}
+
